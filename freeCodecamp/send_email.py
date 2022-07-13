f9cd12e77794048881a2a8a17e01e6dea93e6886
Can Erdoğan <thecanerdogan@gmail.com>
2022-07-13 22:51:11 +0300

    13.07

---
+++ freeCodecamp/send_email.py
@@ -0,0 +1,38 @@
+''' mails from gmail account
+go manage your google account > manage security > less secure app access > on
+'''
+
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = "Email from Python"
+body = " This is a test email form Python!"
+sender_email = "thecanerdogan@gmail.com"
+receiver_email = "thecanerdogan@gmail.com"
+password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
+message.add_alternative(html, subtype="html")
+
+context = ssl.create_default_context()
+
+print("Sending Email!")
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string)
+print("Success")
